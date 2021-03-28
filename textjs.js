6079801885c5916af6bfadb41f673d851a81962d
lucaswonghkg <77853676+lucaswonghkg@users.noreply.github.com>
2021-03-29 00:23:40 +0800

    Add files via upload

---
+++ textjs.js
@@ -0,0 +1,59 @@
+
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+
+
+    
+        if (this.readyState == 4 && this.status == 200)
+        
+        {
+            let data = JSON.parse(this.responseText).feed.
+            entry;
+            var rollText = data[0]["gsx$a"]["$t"];
+     
+     document.getElementById("demo1").innerHTML +=
+
+ "<div>" +
+
+ rollText +
+
+ "</div>"
+
+             
+    }
+
+
+    {
+        let dataa = JSON.parse(this.responseText).feed.
+        entry;
+        var rollText = dataa[0]["gsx$a"]["$t"];
+ 
+ document.getElementById("demo2").innerHTML +=
+
+"<div>" +
+
+rollText +
+
+"</div>"
+
+         
+}
+
+
+
+   
+        
+    
+    };
+    
+    xmlhttp.open(
+    "GET",
+    "https://spreadsheets.google.com/feeds/list/1nHfof0d4jRFoTM7UdUqZ0GGVi388_62EMAfuSr6fJKo/1/public/values?alt=json",
+    true
+    
+    );
+    xmlhttp.send();
+    
+    
+    
